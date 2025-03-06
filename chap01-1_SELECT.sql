3387f4ed7d85b391d0628d28bc6d3bc9de833674
Jongkyun <hjk326333@gmail.com>
2025-03-06 12:43:09 +0900

    SELECT 연습문제(진행중)

--- chap01-1_SELECT.sql
+++ chap01-1_SELECT.sql
@@ -1,20 +1,33 @@
 
 --1. 모든 사원의 사원번호, 이름, 입사일, 급여를 출력하세요.
-
+SELECT employee_id, first_name, hire_date, salary 
+FROM employees;
 
 --2. 모든 사원의 이름과 성을 붙여 출력하세요. 열 별칭은 name으로 하세요.
-
+SELECT first_name || last_name as name
+FROM employees;
 
 --3. 50번 부서 사원의 모든 정보를 출력하세요.
-
+SELECT
+    *
+FROM employees
+WHERE department_id = 50;
 
 --4. 50번 부서 사원의 이름, 부서번호, 직무아이디를 출력하세요.
+SELECT
+    department_name, department_id, location_id
+FROM departments
+WHERE department_id = 50;
 
 
 --5. 모든 사원의 이름, 급여 그리고 300달러 인상된 급여를 출력하세요.
-
+SELECT first_name, salary, salary +300 as 인상된급여
+FROM employees;
 
 --6. 급여가 10000보다 큰 사원의 이름과 급여를 출력하세요.
+SELECT first_name, salary
+FROM employees
+WHERE salary > 10000;
 
 
 --7. 보너스를 받는 사원의 이름과 직무, 보너스율을 출력하세요.
